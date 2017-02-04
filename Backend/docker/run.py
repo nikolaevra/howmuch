f154d5fc86af6f70050a3d6305fe27bb1a247638
nikolaevra <nikolaevra@gmail.com>
2017-02-04 14:13:34 -0500

    Trying something new

--- Backend/docker/run.py
+++ Backend/docker/run.py
@@ -1,13 +1,33 @@
+import httplib
+
 import flask
+from flask import request
 from flask_restful import Resource, Api
 
 app = flask.Flask(__name__)
 api = Api(app)
 
 
+def curl_request(lon, lat):
+    headers = {
+        'user-key': '892a31736bd16f15eedd942201d67ca2',
+    }
+    payload = "?lat={lat}&lon={lon}&q=pizza".format(lat=lat, lon=lon)
+
+    conn = httplib.HTTPConnection("https://developers.zomato.com/api/v2.1/search?")
+    conn.request("GET", "", payload, headers)
+    response = conn.getresponse()
+
+    return response
+
+
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'world'}
+        args = request.args
+        print (args)  # For debugging
+        lon = args['lon']
+        lat = args['lat']
+        return flask.jsonify(curl_request(lon, lat))
 
 api.add_resource(HelloWorld, '/')
 
