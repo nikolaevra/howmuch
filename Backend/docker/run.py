7e59cfc7d871d55e0500c64bd38a0c91a990f50f
soheil-hamidi <soheilhamidi@me.com>
2017-02-04 16:39:16 -0500

    update

--- Backend/docker/run.py
+++ Backend/docker/run.py
@@ -12,15 +12,40 @@ def getLocation():
     radius = request.form['radius']
     price = request.form['price']
 
-    return curl_request(lat, lon)
+    return curl_request(lat, lon, radius, price)
 
-def curl_request(lat, lon):
+# give it an object, that we just parsed from zomato
+def parse_data(restaurants, price):
+    results = []
+
+    for restaurant in restaurants['restaurants']:
+        result = {}
+        item = restaurant['restaurant']
+
+        result['name'] = item['name']
+        result['location'] = item['location']
+
+        # check if the price is below allowed
+        if item['average_cost_for_two'] + item['price_range'] < price:
+            result['price'] = item['average_cost_for_two'] + item['price_range']
+
+        result['rating'] = item['user_rating']
+        result['photo_url'] = item['photos_url'][0]
+        result['menu_url'] = item['menu_url']
+
+        results.append(result)
+
+    return json.dumps({'results': results})
+
+def curl_request(lat, lon, radius, price):
     headers = {
         'content-type': 'application/json',
         'user-key': '892a31736bd16f15eedd942201d67ca2'
         }
 
-    url = 'https://developers.zomato.com/api/v2.1/search?lat={lat}&lon={lon}&q=pizza'.format(lat=lat, lon=lon)
+    url = ('https://developers.zomato.com/api/v2.1/search'
+           '?lat={lat}&lon={lon}&radius={rad}&sort=cost&'
+           'order=asc').format(lat=lat, lon=lon, rad=radius)
 
     try:
         r = requests.get(url, headers=headers).content
