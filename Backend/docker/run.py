e400adfdceeacf79b375e357e3c8e66fad9e4a68
soheil-hamidi <soheilhamidi@me.com>
2017-02-04 15:19:12 -0500

    update to run.py

--- Backend/docker/run.py
+++ Backend/docker/run.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import json
 
 app = Flask(__name__)
 
@@ -7,8 +8,23 @@ def getLocation():
     lat = request.form['lat']
     lon = request.form['lon']
     price = request.form['price']
-    
-    return jsonify(lat + " " + lon + " " + price)
+
+    return curl_request(lat, lon)
+
+def curl_request(lat, lon):
+    headers = {
+        'content-type': 'application/json',
+        'user-key': '892a31736bd16f15eedd942201d67ca2'
+        }
+
+    url = 'https://developers.zomato.com/api/v2.1/search?lat={lat}&lon={lon}&q=pizza'.format(lat=lat, lon=lon)
+
+    try:
+        r = requests.get(url, headers=headers)
+        return r.json()
+    except URLError, e:
+        return jsonify('No API!'), e
 
 if __name__ == '__main__':
+    app.debug=True
     app.run(host='0.0.0.0', port=5000)
