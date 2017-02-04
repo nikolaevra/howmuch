39830076a7408aa50eb01c007ae6b29fa63e5925
Subhan Nadeem <subhan97@hotmail.com>
2017-02-04 14:16:44 -0500

    Added full maps functionality

---
+++ app/src/main/java/me/snadeem/howmuch/LoginActivity.java
@@ -0,0 +1,221 @@
+package me.snadeem.howmuch;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "Testing: ";
+    public static String KEY_SKIP_LOGIN = "SKIP_LOGIN";
+    public static String KEY_IS_LOGGED_IN = "IS_LOGGED_IN";
+    private CallbackManager mCallbackManager = CallbackManager.Factory.create();
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    public static String facebookName;
+    public static String imageURL;
+    private SharedPreferences prefs;
+
+    public static void fadeIn(final View view) {
+        view.setVisibility(View.VISIBLE);
+        view.setAlpha(0);
+        final int DURATION = 1000;
+        view.animate().setDuration(DURATION).alpha(1).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                view.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Initialize Facebook SDK & logger
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+        setContentView(R.layout.activity_login);
+        fadeIn(findViewById(R.id.activity_login));
+
+         prefs = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE);
+
+        // Set custom text
+/*
+        Typeface myTypeface = Typeface.createFromAsset(getAssets(), "coolvetica.ttf");
+        TextView myTextView = (TextView)findViewById(R.id.appText);
+        myTextView.setTypeface(myTypeface);
+*/
+
+        initializeLogInButton();
+        initializeFirebaseAuthorization();
+
+        // Show access button if user is logged in
+        if (isLoggedIn() || skipLoginEnabled()) {
+            goToMainActivity();
+        }
+        else { // User is logged out
+            FirebaseAuth.getInstance().signOut();
+            LoginManager.getInstance().logOut();
+            prefs.edit().clear().apply();
+        }
+
+        Button skipButton = (Button) findViewById(R.id.skipLoginButton);
+
+        skipButton.setOnClickListener(view -> {
+            prefs.edit().putBoolean(KEY_SKIP_LOGIN, true).apply();
+            goToMainActivity();
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+/*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.log_in, menu);
+        return true;
+    }*/
+
+/*
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+*/
+
+
+    private void initializeLogInButton() {
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("public_profile");
+
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+                Toast.makeText(LoginActivity.this, "Authentication cancelled.",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError", error);
+                // ...
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        // Pass the activity result back to the Facebook SDK
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    Log.d(TAG, "successfulFacebookAuth: " + task.isSuccessful());
+
+                    // If sign in fails, display a message to the user. If sign in succeeds
+                    // the auth state listener will be notified and logic to handle the
+                    // signed in user can be handled in the listener.
+                    if (!task.isSuccessful()) {
+                        Log.w(TAG, "signInWithCredential", task.getException());
+                        Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        prefs.edit().putBoolean(KEY_IS_LOGGED_IN, true).apply();
+                        goToMainActivity();
+                    }
+                });
+    }
+
+    private void goToMainActivity() {
+        Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+        facebookName =  FirebaseAuth.getInstance().getCurrentUser().getDisplayName();
+        imageURL = FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl().toString();
+        startActivity(intent);
+    }
+
+    private boolean isLoggedIn() {
+        return AccessToken.getCurrentAccessToken() != null;
+    }
+
+    private boolean skipLoginEnabled() {
+        return prefs.getBoolean(KEY_SKIP_LOGIN, false);
+    }
+
+    private void initializeFirebaseAuthorization() {
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = firebaseAuth -> {
+            FirebaseUser user = firebaseAuth.getCurrentUser();
+            if (user != null) {
+                // User is signed in
+                Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+            } else {
+                // User is signed out
+                Log.d(TAG, "onAuthStateChanged:signed_out");
+            }
+            // ...
+        };
+    }
+}
--- app/src/main/java/me/snadeem/howmuch/MainActivity.java
+++ app/src/main/java/me/snadeem/howmuch/MainActivity.java
@@ -1,13 +1,22 @@
 package me.snadeem.howmuch;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+import static me.snadeem.howmuch.R.id.amountEdit;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String AMOUNT = "amount";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
