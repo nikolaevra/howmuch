dffeed5f36c012566b2ead43ae05a8be5da8d6ec
soheil-hamidi <soheilhamidi@me.com>
2017-02-04 15:04:51 -0500

    run.py update

--- Backend/docker/run.py
+++ Backend/docker/run.py
@@ -1,13 +1,15 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/get-location', methods=['POST'])
-def createCategory():
-    lat=request.form['lat']
-    lon=request.form['lon']
-    price=request.form['price']
-    print(lat + " " + lon + " " + price)
+def getLocation():
+    lat = request.form['lat']
+    lon = request.form['lon']
+    price = request.form['price']
+    # print(lat + " " + lon + " " + price)
+
+    return jsonify(lat + " " + lon + " " + price)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
